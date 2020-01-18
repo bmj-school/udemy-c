6a3289514d4b015f6e9967a09223aa3aebc66c18
MarcusJones <jones.0bj3@gmail.com>
2020-01-18 20:46:09 +0100

    add primes works

--- l59-prime-nums/main.c
+++ l59-prime-nums/main.c
@@ -6,7 +6,7 @@
 #define SIZE_PRIMES_ARRAY 100
 int main()
 {
-    int primes[SIZE_PRIMES_ARRAY] = {1, 2, 3};
+    int primes[SIZE_PRIMES_ARRAY] = {2, 3};
     int p;
     double remainder;
     for (int num=5; num<MAX_NUM; num+=2){
@@ -14,16 +14,23 @@ int main()
         for (int i=0; i<SIZE_PRIMES_ARRAY; i++){
             // Checked every previous prime - therefore this is definately prime!
             if (primes[i]==0) {
+                printf("*** %d is PRIME!!! ***\n", num);
                 primes[i]=num;
                 break;
             }
+            printf("%d/%d ",num, primes[i]);
             remainder = num%primes[i];
-            printf("Num: %3d, Index: %2d, Prime: %5d, Rem: %0.2f", num, i, primes[i], remainder);
-            printf("\n");
 
+            // printf("Num: %3d, Index: %2d, Prime: %5d, Rem: %0.2f", num, i, primes[i], remainder);
+            // printf("\n");
+
+            // If num is divisible, it's definately NOT prime, break and don't add to list!
             if (remainder == 0.0) {
+                printf("NOT PRIME!\n");
                 break;
             }
+            printf("\n");
+
             // printf("Remainder? %d", remainder>0.0);
             // if (remainder > 0.0) {
             //     primes[i] = num;
