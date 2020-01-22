00eb23c0fd6001ac9e5ef3e25806249bb51242c1
MarcusJones <jones.0bj3@gmail.com>
2020-01-22 21:37:11 +0100

    add malloc code

---
+++ l105-malloc/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char *str;
+
+    str = (char *) malloc(15 * sizeof(char));
+    strcpy(str, "hello");
+    printf("String = %s, Address = %p  %p\n", str, str, &str);
+
+    str = (char *) realloc(str, 25*sizeof(char));
+    strcat(str, ".com");
+    printf("String = %s, Address = %p  %p\n ", str, str, &str);
+
+    return 0;
+}
