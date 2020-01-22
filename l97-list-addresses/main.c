c79525cf113f63564371d14e9913bcf875c54477
MarcusJones <jones.0bj3@gmail.com>
2020-01-22 10:45:59 +0100

    add list addresses

---
+++ l97-list-addresses/main.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    char multiple[] = "a string";
+    char * p = multiple;
+    for (i = 0; i<strlen(multiple); ++i)
+    {
+        printf("multiple[%d]=%c, *(p+%d)=%c",
+            i, multiple[i],
+            i, *(p+i));
+        printf("&multiple[%d]=%p, p+%d=%p\n",
+            i, &multiple[i], i, p+i);
+    }
+
+    return 0;
+}
