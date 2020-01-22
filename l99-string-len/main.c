641f11a5df0075f727b987f8d6a1e8888dc8cd65
MarcusJones <jones.0bj3@gmail.com>
2020-01-22 11:29:10 +0100

    add char counter

---
+++ l99-string-len/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void copyStringArray(char to[], char from[])
+{
+    int i;
+    for (i=0; from[i]!='\0'; ++i)
+        to[i] = from[i];
+    to[i] = '\0';
+}
+
+void copyStringPtr(char *to, char *from)
+{
+    for ( ; *from!='\0', ++from; ++to)
+        *to = *from;
+    *to='\0';
+}
+
+void copyStringPtrWhile(char *to, char *from)
+{
+    while (*from) // Will break on the Null
+        *to++ = *from++;
+    *to = '\0';
+}
+
+int myStringLen(char *str)
+{
+    int count = 0;
+    while(*str)
+    {
+        count++;
+        *str++;
+    }
+    return count;
+}
+int main()
+{
+    char str1[] = "A 0 string to be copied";
+    char str2[50];
+
+    copyStringPtrWhile(str2, str1);
+    printf("%s\n%s\n", str1, str2);
+
+    printf("'%s' has %i characters", str1, myStringLen(str1));
+
+    return 0;
+}
